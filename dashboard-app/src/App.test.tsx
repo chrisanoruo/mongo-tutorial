1640d52d113c72e433bf2b6ac916459cd97af9b4
toptalentdev81 <goldhorse@GoldHorses-Mac.local>
2019-08-12 05:25:40 -0700

    Fix test code

--- dashboard-app/src/App.test.tsx
+++ dashboard-app/src/App.test.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { shallow } from 'enzyme';
 import App from './App';
 import Adapter from 'enzyme-adapter-react-16';
 import { configure, shallow } from 'enzyme';
 
 configure({ adapter: new Adapter() });
-
-// const App = () => {};
-
-// it('should be ok', () => {
-//   const flag = 1;
-//   expect(flag).toBe(1);
-// })
-
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
 
 describe('App', () => {
   it('renders correctly', () => {
