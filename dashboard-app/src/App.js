087393d8e8ee16ca48dbf272a66c0321709a3448
toptalentdev81 <christopheranoruo81@gmail.com>
2019-08-12 10:46:38 +0800

    sample code

--- dashboard-app/src/App.js
+++ dashboard-app/src/App.js
@@ -1,124 +1,51 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import { Row, Col, Card, Layout, Spin, Statistic, Table } from "antd";
+import ReactDOM from "react-dom";
+
 import "antd/dist/antd.css";
-import "./index.css";
+import { Row, Col, Card, Layout, Spin } from "antd";
+
 import cubejs from "@cubejs-client/core";
 import { QueryRenderer } from "@cubejs-client/react";
 import { Chart, Axis, Tooltip, Geom, Coord, Legend } from "bizcharts";
-import moment from "moment";
 
-const AppLayout = ({ children }) => (
-  <Layout>
-    <Layout.Header>
-      <div
-        style={{
-          float: "left"
-        }}
-      >
-        <h2
-          style={{
-            color: "#fff",
-            margin: 0,
-            marginRight: "1em"
-          }}
-        >
-          My Dashboard
-        </h2>
-      </div>
-    </Layout.Header>
-    <Layout.Content
-      style={{
-        padding: "0 25px 25px 25px",
-        margin: "25px"
-      }}
-    >
-      {children}
-    </Layout.Content>
-  </Layout>
-);
-
-const Dashboard = ({ children }) => (
-  <Row type="flex" justify="space-around" align="top" gutter={24}>
-    {children}
-  </Row>
-);
-
-const DashboardItem = ({ children, title }) => (
-  <Col span={24} lg={12}>
-    <Card
-      title={title}
-      style={{
-        marginBottom: "24px"
-      }}
-    >
-      {children}
-    </Card>
-  </Col>
-);
-
-const stackedChartData = resultSet => {
-  const data = resultSet
-    .pivot()
-    .map(({ xValues, yValuesArray }) =>
-      yValuesArray.map(([yValues, m]) => ({
-        x: resultSet.axisValuesString(xValues, ", "),
-        color: resultSet.axisValuesString(yValues, ", "),
-        measure: m && Number.parseFloat(m)
-      }))
-    )
-    .reduce((a, b) => a.concat(b));
-  return data;
-};
-
-const pieRender = ({ resultSet }) => (
+const renderChart = resultSet => (
   <Chart height={400} data={resultSet.chartPivot()} forceFit>
     <Coord type="theta" radius={0.75} />
-    {resultSet.seriesNames().map(s => (
-      <Axis name={s.key} />
-    ))}
-    <Legend position="right" />
-    <Tooltip />
-    {resultSet.seriesNames().map(s => (
-      <Geom type="intervalStack" position={s.key} color="category" />
-    ))}
+    <Axis name="Zips.count" />
+    <Legend position="bottom" name="category" />
+    <Tooltip showTitle={false} />
+    <Geom type="intervalStack" position="Zips.count" color="x" />
   </Chart>
 );
 
-const API_URL = "http://localhost:4000";
 const cubejsApi = cubejs(
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NjU1NzY0NjMsImV4cCI6MTU2NTY2Mjg2M30.OIF1sF9ROBCl_PNdxQrggOl9hCnCGY7QN7MVf-khSWs",
-  {
-    apiUrl: API_URL + "/cubejs-api/v1"
-  }
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1NDkzMDk3NzMsImV4cCI6MTU0OTM5NjE3M30.eXEdfUa_ek2V9MlGTpBMOd_AFfs8laaZj8ZsuM1wqqo",
+  { apiUrl: "http://localhost:4000/cubejs-api/v1" }
 );
 
-const renderChart = Component => ({ resultSet, error }) =>
-  (resultSet && <Component resultSet={resultSet} />) ||
-  (error && error.toString()) || <Spin />;
+const { Header, Footer, Sider, Content } = Layout;
 
-function App() {
-  return (
-    <div className="App">
-      <AppLayout>
-        <Dashboard>
-          <DashboardItem>
+const App = () => (
+  <Layout>
+    <Header>
+      <h2 style={{ color: "#fff" }}>MongoDB Dashboard</h2>
+    </Header>
+    <Content style={{ padding: "25px", margin: "25px" }}>
+      <Row type="flex" justify="space-around" align="top" gutter={24}>
+        <Col span={24} lg={12}>
+          <Card title="Zip count by state" style={{ marginBottom: "24px" }}>
             <QueryRenderer
-              query={{
-                measures: ["Zips.count"],
-                timeDimensions: [],
-                dimensions: ["Zips.state"],
-                filters: []
-              }}
+              query={{ measures: ["Zips.count"], dimensions: ["Zips.state"] }}
               cubejsApi={cubejsApi}
-              render={renderChart(pieRender)}
-            />
-          </DashboardItem>
-        </Dashboard>
-      </AppLayout>
-    </div>
-  );
-}
-
+              render={({ resultSet, error }) =>
+               (resultSet && renderChart(resultSet)) ||
+(error && error.toString()) || <Spin />
+              }
+              />
+          </Card>
+        </Col>
+      </Row>
+    </Content>
+  </Layout>
+);
 export default App;
